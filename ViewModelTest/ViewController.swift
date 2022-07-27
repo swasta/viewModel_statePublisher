5156de15e180a41975bcab762bbfa206b7bbad18
Nikita Borodulin <link.bor@gmail.com>
2022-07-27 19:55:13 +0300

    Initial Commit

---
+++ ViewModelTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Created by Nikita Borodulin on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
